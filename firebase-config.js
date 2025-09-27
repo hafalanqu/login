9ea8a027f6c2ea4da63ab59c012e40a3dc80de60
hafalanqu <hafalanqu@users.noreply.github.com>
2025-09-27 22:55:20 +0000

    deploy: dc45de486ec5b30c0820a70ec6a9d42be0f91328

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyB4zxl2TCsevr7KKfdpIQYE3VRyfr-rLOY',
-  authDomain: 'hafalanqu-app.firebaseapp.com',
-  projectId: 'hafalanqu-app',
-  storageBucket: 'hafalanqu-app.firebasestorage.app',
-  messagingSenderId: '1032716870319',
-  appId: '1:1032716870319:web:30bd0e7353ae806f676a5a',
-};
